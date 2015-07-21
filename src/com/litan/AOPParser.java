492e67129c044c7642213709621bf68e4fa4b9e8
litan <liyan12167@163.com>
2015-07-21 22:41:47 +0800

    使用Log来替换Syso来打印

--- src/com/litan/AOPParser.java
+++ src/com/litan/AOPParser.java
@@ -14,44 +14,45 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class AOPParser {
-    private static final String FILE = "f";
-    public static void main(String[] args) throws ParseException {
-        Options opts = new Options();
-        Option opt = new Option(FILE, true, "指定数据源文件");
-        opts.addOption(opt);
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cl = parser.parse(opts, args);
-        if (cl.hasOption(FILE)) {
-            String file = cl.getOptionValue(FILE);
-            System.out.println("file:" + file);
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(new File(file)));
-                String line;
-                ParsedFlow flow = new ParsedFlow();
-                try {
+	private static final String FILE = "f";
+
+	public static void main(String[] args) throws ParseException {
+		Options opts = new Options();
+		Option opt = new Option(FILE, true, "指定数据源文件");
+		opts.addOption(opt);
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cl = parser.parse(opts, args);
+		if (cl.hasOption(FILE)) {
+			String file = cl.getOptionValue(FILE);
+			Log.d("file:" + file);
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(new File(
+						file)));
+				String line;
+				ParsedFlow flow = new ParsedFlow();
+				try {
 					while ((line = br.readLine()) != null) {
-					    System.out.println(line);
-					    if (line.startsWith("-")) {
-					        continue;
-					    }
-					    flow.parseLine(line);
+						Log.v(line);
+						if (line.startsWith("-")) {
+							continue;
+						}
+						flow.parseLine(line);
 					}
 				} catch (ParsedException e) {
-					System.err.println("遭遇异常");
 					e.printStackTrace();
 				}
-                line = br.readLine();
-            } catch (FileNotFoundException e) {
-                System.err.println("文件未找到:" + file);
-                return;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
-        } else {
-            System.err.println("-f 是必须的,用来制定数据源文件");
-            return;
-        }
-//        opt = new Option("threadTime", false, "按方法执行的平均线程消耗时间从高到低显示前十个");
-    }
+				line = br.readLine();
+			} catch (FileNotFoundException e) {
+				Log.e("文件未找到:" + file);
+				return;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		} else {
+			Log.e("-f 是必须的,用来制定数据源文件");
+			return;
+		}
+		// opt = new Option("threadTime", false, "按方法执行的平均线程消耗时间从高到低显示前十个");
+	}
 }
--- src/com/litan/Log.java
+++ src/com/litan/Log.java
@@ -1,6 +1,14 @@
 package com.litan;
 
 public class Log {
+	private static final boolean DBG = true;
+
+	static void v(String msg) {
+		if (DBG) {
+			System.out.println("VERBOSE:" + msg);
+		}
+	}
+
 	static void d(String msg) {
 		System.out.println(msg);
 	}
