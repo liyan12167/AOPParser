1dfa02d95585bf0e977a56834792c65021818a1e
litan <liyan12167@163.com>
2015-07-21 22:06:31 +0800

    定义解析流程

--- src/com/litan/ParsedFlow.java
+++ src/com/litan/ParsedFlow.java
@@ -1,33 +1,33 @@
 package com.litan;
 
 public class ParsedFlow {
-    private static final int LINE_TIME = 0;
-    private static final int LINE_LOG = 0;
-    private static final int LINE_BR = 0;
-    private int mLine = LINE_TIME;
+	private static final int LINE_TIME = 0;
+	private static final int LINE_LOG = 1;
+	private static final int LINE_BR = 2;
+	private int mLine = LINE_TIME;
 
-    public void parseLine(String line) throws ParsedException {
-        if (mLine == LINE_TIME) {
+	public void parseLine(String line) throws ParsedException {
+		if (mLine == LINE_TIME) {
+			parseTimeLine(line);
+			mLine++;
+		} else if (mLine == LINE_TIME) {
+			parseLogLine(line);
+			mLine++;
+		} else if (mLine == LINE_TIME) {
+			parseBRLine(line);
+			mLine = LINE_TIME;
+		}
+	}
 
-        } else if (mLine == LINE_TIME) {
+	private void parseTimeLine(String line) throws ParsedException {
 
-        } else if (mLine == LINE_TIME) {
+	}
 
-        }
-        parseTimeLine();
-        parseLogLine();
-        parseBRLine();
-    }
+	private void parseLogLine(String line) throws ParsedException {
 
-    private void parseTimeLine() throws ParsedException {
+	}
 
-    }
+	private void parseBRLine(String line) throws ParsedException {
 
-    private void parseLogLine() throws ParsedException {
-
-    }
-
-    private void parseBRLine() throws ParsedException {
-
-    }
+	}
 }
