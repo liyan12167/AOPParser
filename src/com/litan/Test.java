a5810472dfd3da1b80ce4c44c904bb8c2482432c
litan <litan@dianxinos.com>
2015-09-02 14:22:47 +0800

    demo insert数据时自增id

--- src/com/litan/Test.java
+++ src/com/litan/Test.java
@@ -24,28 +24,28 @@ public class Test {
 
 	    Statement stat = conn.createStatement();
 
-	    stat.executeUpdate("create table tbl1(name varchar(20), salary int);");// 创建一个表，两列
+	    stat.executeUpdate("create table tbl1(id INTEGER PRIMARY KEY,name varchar(20), salary int);");// 创建一个表，两列
 	    conn.setAutoCommit(false);
-	    for (int i = 0; i < 200; i++) {
+	    for (int i = 0; i < 2; i++) {
 //		stat.executeUpdate("insert into tbl1 values('ZhangSan',8000);"); // 插入数据
 //		stat.execute("insert into tbl1 values('ZhangSan',8000);"); // 插入数据
-		stat.addBatch("insert into tbl1 values('ZhangSan',8000);");
+		stat.addBatch("insert into tbl1(name,salary) values('ZhangSan',8000);");
 	    }
 	    stat.executeBatch();
 //	    stat.executeUpdate("insert into tbl1 values('LiSi',7800);");
 //	    stat.executeUpdate("insert into tbl1 values('WangWu',5800);");
 //	    stat.executeUpdate("insert into tbl1 values('ZhaoLiu',9100);");
 
-//	    ResultSet rs = stat.executeQuery("select * from tbl1;"); // 查询数据
-//
-//	    while (rs.next()) { // 将查询到的数据打印出来
-//
-//		System.out.print("name = " + rs.getString("name") + " "); // 列属性一
-//
-//		System.out.println("salary = " + rs.getString("salary")); // 列属性二
-//
-//	    }
-//	    rs.close();
+	    ResultSet rs = stat.executeQuery("select * from tbl1;"); // 查询数据
+
+	    while (rs.next()) { // 将查询到的数据打印出来
+		System.out.println("id=" + rs.getInt("id"));
+		System.out.print("name = " + rs.getString("name")); // 列属性一
+
+		System.out.println("salary = " + rs.getString("salary")); // 列属性二
+
+	    }
+	    rs.close();
 	    conn.close(); // 结束数据库的连接
 	    System.out.println("==endt==");
 	    long t2 = System.currentTimeMillis();
